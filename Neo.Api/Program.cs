424fbfc038a485445d47a7ecf40ba2cb9bb38e08
GoldinBaloyi <goldin.baloyi@za.zetes.com>
2025-06-30 23:13:50 +0200

    added project depency injection with app.json settings

--- Neo.Api/Program.cs
+++ Neo.Api/Program.cs
@@ -1,3 +1,13 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Neo.Application.Validation;
+using Neo.Infrastructure.Auth;
+using Neo.Infrastructure.Data;
+using Neo.Infrastructure.Repositories;
+using System.Text;
+using Neo.Domain.Interfaces;
 
 namespace Neo.Api
 {
@@ -8,11 +18,47 @@ namespace Neo.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            // Swagger/OpenAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // ---------- Application & CQRS ----------
+            // MediatR: scan Neo.Application for handlers
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Neo.Application.UseCases.CreatePost.CreatePostHandler).Assembly));
+            // FluentValidation: scan Neo.Application for validators
+            builder.Services.AddValidatorsFromAssembly(typeof(Neo.Application.UseCases.CreatePost.CreatePostCommandValidator).Assembly);
+            // MediatR pipeline: add FluentValidation behavior
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            // ---------- Infrastructure/Repositories ----------
+            builder.Services.AddSingleton<DbContext>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddScoped<IPostLikeRepository, PostLikeRepository>();
+            builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<ITagRepository, TagRepository>();
+            builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+            // ---------- JWT Auth ----------
+            var jwtSection = builder.Configuration.GetSection("JwtSettings");
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSection["Issuer"],
+                        ValidAudience = jwtSection["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["Secret"]!)),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            builder.Services.AddAuthorization(); 
 
             var app = builder.Build();
 
@@ -25,10 +71,12 @@ namespace Neo.Api
 
             app.UseHttpsRedirection();
 
+            // Add authentication and authorization middleware
+            app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
 
             app.Run();
         }
